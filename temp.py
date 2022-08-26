3e1a6e85f286ca98a3972d03cd29b8eb35e45acd
Aidan Semancik <903140@H2WHK18GQ6NW.tld>
2022-08-26 11:32:34 -0500

    added while loop and isdigit method to ensure a number is entered

--- temp.py
+++ temp.py
@@ -1,12 +1,17 @@
 name = input("What's your name?")
 print('Hello ', name, '!')
 
-begin_temp = int(input('Beginning temperature?'))
+begin_temp = input('What is your beginning temperature?')
+
+while not begin_temp.isdigit():
+    begin_temp = input('Please enter a number.')
+
 begin_unit = input('In C or F?')
 
 if begin_unit == ('C' or 'c' or 'celsius' or 'Celsius'):
-    end_temp = (begin_temp * 1.8) + 32
+    end_temp = (int(begin_temp) * 1.8) + 32
     print("Hey ", name, ", ", begin_temp, "°C", " in Farenheit is ", end_temp, "°F.")
 elif begin_unit == ('F' or 'f' or 'farenheit' or 'Farenheit'):
-    end_temp = (begin_temp - 32) * .5556
+    end_temp = (int(begin_temp) - 32) * .5556
     print("Hey ", name, ", ", begin_temp, "°F", " in Celsius is ", end_temp, "°C.")
+
